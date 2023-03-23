8e5b66a7e170b507a03d5c81c16f80c940468ca6
Jadyn Gomez <jadyngg19@gmail.com>
2023-03-23 19:01:16 -0400

    Changing some of the script

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Wait for the page to finish loading before running the code
+$(document).ready(function() {
+  // Display the current date and time at the top of the page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY, h:mm A"));
+
+  // Get the current hour in 24-hour format
+  const currentHour = dayjs().hour();
+
+  // Loop through each time-block div and apply the appropriate class based on the current hour
+  $(".time-block").each(function() {
+    // Extract the hour from the id attribute of the time-block div
+    const hour = parseInt($(this).attr("id").split("-")[1]);
+
+    // If the hour is before the current hour, add the "past" class
+    if (hour < currentHour) {
+      $(this).addClass("past").removeClass("present future");
+    }
+    // If the hour is the same as the current hour, add the "present" class
+    else if (hour === currentHour) {
+      $(this).addClass("present").removeClass("past future");
+    }
+    // If the hour is after the current hour, add the "future" class
+    else {
+      $(this).addClass("future").removeClass("past present");
+    }
+  });
+
+  // Save button event listener
+  $(".saveBtn").on("click", function() {
+    // Get the value of the sibling textarea
+    const text = $(this).siblings(".description").val();
+    // Get the id of the parent time-block div
+    const id = $(this).parent().attr("id");
+    // Save the text and id to local storage
+    localStorage.setItem(id, text);
+  });
+
+  // Loop through each time-block div and retrieve its saved text from local storage
+  $(".time-block").each(function() {
+    const id = $(this).attr("id");
+    const text = localStorage.getItem(id);
+    // If there is saved text, display it in the textarea
+    if (text !== null) {
+      $(this).children(".description").val(text);
+    }
+  });
 });
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Task-Master
+
+## Description
+
+## Usage
+
+## Link
